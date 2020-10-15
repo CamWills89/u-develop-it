70f88cddb5c049615bed329e89cc21f2f75d771b
Cameron <cameron.wills89@outlook.com>
2020-10-15 09:42:49 -0400

    modulairxed code, and added in voters section with api routes

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,8 @@
+--delete the current data if it exist, so we can remake the tables
 DROP TABLE IF EXISTS parties;
 DROP TABLE IF EXISTS candidates;
+DROP TABLE IF EXISTS voters;
+--parties table needs to come before the candidates because of the constraint and foreign key
 CREATE TABLE parties (
     id INTEGER PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
@@ -13,4 +16,11 @@ CREATE TABLE candidates (
     party_id INTEGER UNSIGNED,
     CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE
     SET NULL
+);
+CREATE TABLE voters (
+    id INTEGER PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
